20d0e06ff15678ba5f2d61a8d383c572c5f61cb0
Prince-Ugwa <odiniyaprinceugwa@gmail.com>
2022-10-25 18:47:36 +0100

    commit

---
+++ starter/read.md
@@ -0,0 +1,5 @@
+This is Workout application to keep track of daily workout and the area which a user has workedout the most.
+
+the features of this app are
+
+1.
---
+++ starter/script.js
@@ -0,0 +1,141 @@
+'use strict';
+
+// prettier-ignore
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
+
+/////////////////////////USING THE GEOLOCATION API//////////////////////
+let map, mapEvent;
+
+class App {
+  constructor() {
+    this._getPosition;
+  }
+  _getPosition() {
+    if (navigator.geolocation)
+      navigator.geolocation.getCurrentPosition(this._loadMap, function () {
+        alert('could not get your position');
+      });
+  }
+  _loadMap(position) {
+    console.log(position);
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+    console.log(`https://www.google.com/maps/@${latitude}, ${longitude}`);
+
+    ///////////////////////////////DISPLAYING A MAP USING LEAFLET////////////////////////////////////
+    const coords = [latitude, longitude];
+    // const map = L.map('map').setView([51.505, -0.09], 13);// the no. 13 the zoom level of the map on the browser
+    map = L.map('map').setView(coords, 14);
+    L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    // HANDLING CLICK ON MAP
+    map.on('click', function (mapE) {
+      mapEvent = mapE;
+      form.classList.remove('hidden');
+      inputDistance.focus();
+    });
+  }
+}
+
+const app = new App();
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  inputDistance.value =
+    inputDuration.value =
+    inputCadence.value =
+    inputElevation.value =
+      '';
+  // DISPLAY MARKER ON THE MAP
+  const { lat, lng } = mapEvent.latlng;
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+      })
+    )
+    .setPopupContent('EXERCISING')
+    .openPopup();
+});
+
+inputType.addEventListener('change', function () {
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+});
+/*/////////////////////////WITHOUT OOP CONCEPT////////////////////////////////
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      console.log(position);
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(`https://www.google.com/maps/@${latitude}, ${longitude}`);
+
+      ///////////////////////////////DISPLAYING A MAP USING LEAFLET////////////////////////////////////
+      const coords = [latitude, longitude];
+      // const map = L.map('map').setView([51.505, -0.09], 13);// the no. 13 the zoom level of the map on the browser
+      map = L.map('map').setView(coords, 14);
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      // HANDLING CLICK ON MAP
+      map.on('click', function (mapE) {
+        mapEvent = mapE;
+        form.classList.remove('hidden');
+        inputDistance.focus();
+        console.log(mapEvent);
+      });
+    },
+    function () {
+      alert('could not get your position');
+    }
+  );
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  inputDistance.value =
+    inputDuration.value =
+    inputCadence.value =
+    inputElevation.value =
+      '';
+  // DISPLAY MARKER ON THE MAP
+  const { lat, lng } = mapEvent.latlng;
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+      })
+    )
+    .setPopupContent('EXERCISING')
+    .openPopup();
+});
+
+inputType.addEventListener('change', function () {
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+});
+
+///////////////////////////////////////////*/ ////////////////////////////////////////
